3d51483a6bef056b5f5e9c4697896104fa798dcc
Shourya742 <sshourya17@gmail.com>
2023-07-30 22:13:20 +0530

    Added Course edit feature

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -35,6 +35,34 @@ const App = () => {
   );
 };
 
-const InitUser = () => {};
+const InitUser = () => {
+  const setUser = useSetRecoilState(userState);
+  const init = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/admin/me`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (response.data.username) {
+        setUser({
+          isLoading: false,
+          userEmail: response.data.username,
+        });
+      } else {
+        setUser({ isLoading: false, userEmail: null });
+      }
+    } catch (error) {
+      setUser({
+        isLoading: false,
+        userEmail: null,
+      });
+    }
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return <></>;
+};
 
 export default App;
